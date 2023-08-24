24a85c7e7e5f562be875122958a7a9cf73aa45e9
Pravin <pravin08pawar@gmail.com>
2023-08-24 11:48:03 +0530

    day-2

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+require("dotenv").config()
+app.use(express.json());
 
+const userRoute = require("./routes/userRoute")
+
+app.get("/",(req,res)=>{
+
+    res.send("welcome");
+})
+
+app.use("/users",userRoute)
+
+
+const connect = async()=>{
+
+    try {
+        
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("connected")
+
+    } catch (error) {
+        
+        console.log(error)
+    }
+}
+app.listen(process.env.PORT,()=>{
+
+    connect();
+
+    console.log("listen")
+})
+
+module.exports = app
---
+++ backend/models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    confirm_password:{type:String,required:true}
+})
+
+const user = mongoose.model("user",userSchema);
+
+module.exports= user;
