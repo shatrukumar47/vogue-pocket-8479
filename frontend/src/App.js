229df55489ef8c4609fbc4e5703a0a92b63d80b3
Shatrughan Kumar <123942835+shatrukumar47@users.noreply.github.com>
2023-08-26 13:56:15 +0530

    some corrections done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import HomePage from './Pages/HomePage';
-import MainRoutes from './Routes/MainRoutes';
-import Footer from './Components/Footer';
-
-
-
-import SimpleSlider from './Components/Carousel';
-
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import MainRoutes from "./Routes/MainRoutes";
+import Footer from "./Components/Footer";
 
 
 function App() {
   return (
     <div className="App">
-    
-    
-       <Navbar />
+      <Navbar />
       <MainRoutes />
-
       <Footer />
-  
-
-
     </div>
   );
 }
